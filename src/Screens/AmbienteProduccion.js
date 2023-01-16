2af82e2f48d220c6552a8ef5ae8901a202864c92
Eliezer Ponce <eliezerponcexd@gmail.com>
2023-01-15 20:31:31 -0600

    Mejoras

--- src/Screens/AmbienteProduccion.js
+++ src/Screens/AmbienteProduccion.js
@@ -1,11 +1,21 @@
 import * as React from "react";
+import react, { useState } from "react";
 import { WebView } from "react-native-webview";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
 import Constants from "expo-constants";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Foundation } from "@expo/vector-icons";
 
 export default function App() {
+  const [visible, setVisible] = useState(true);
+
   return (
     <>
       <View style={styles.textView}>
@@ -21,7 +31,56 @@ export default function App() {
       <WebView
         style={styles.container}
         source={{ uri: "http://192.168.2.102:81/login" }}
+        onLoadEnd={() => setVisible(false)}
+        size={"large"}
+        scalesPageToFit={true}
+        renderError={() => {
+          return (
+            <View
+              style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: "white",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <ImageBackground
+                style={styles.image}
+                source={require("../Images/LOGO.png")}
+              />
+              <Text
+                style={{
+                  color: "black",
+                  justifyContent: "center",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontSize: 32,
+                }}
+              >
+                Error al cargar la aplicacion
+              </Text>
+              <Text
+                style={{
+                  color: "black",
+                  justifyContent: "center",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontSize: 32,
+                  margin: 10,
+                }}
+              >
+                Porfavor conectese a una red de Joya de Nicaragua o Habilite la
+                VPN
+              </Text>
+            </View>
+          );
+        }}
       />
+
+      {visible && (
+        <ActivityIndicator color={"red"} style={styles.ActivityIndicator} />
+      )}
     </>
   );
 }
@@ -51,5 +110,11 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "yellow",
     bottom: 25,
+  },
+
+  ActivityIndicator: {
+    position: "absolute",
+    top: Dimensions.get("screen").height / 2,
+    left: Dimensions.get("screen").width / 2.1,
   },
 });
